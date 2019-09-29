cc5a82c3e6bc4dbf205dbe901fa33c1a7013023c
“woofborn” <wolf.born@gmail.com>
2019-09-29 10:59:38 +0800

    Basic mechanics for advancing pages functional. Some issues with shuffled array.

--- script.js
+++ script.js
@@ -23,35 +23,26 @@ function homeScreen(){
     home.setAttribute("id", "homepage")
     body.appendChild(home)
 //creates image background
-
     var backgroundDiv = document.createElement("div")
-
     backgroundDiv.style.backgroundImage = `url("images/backgroundblur.jpg")`
     backgroundDiv.setAttribute("class", "col-12 d-flex justify-content-center")
     backgroundDiv.style.height = "100vh"
-
     home.appendChild(backgroundDiv)
-
 //creates welcome message
-
     var messageSpan = document.createElement("span")
     messageSpan.setAttribute("id", "homemessage")
     messageSpan.setAttribute("class", "col-12 d-flex justify-content-center")
     messageSpan.innerText = "Welcome to TypeQuest!"
     backgroundDiv.appendChild(messageSpan)
-    // typeWriter()
-
 //creates button div
     var buttonDiv = document.createElement("div")
     buttonDiv.setAttribute("class", "col-8 d-flex justify-content-center")
     messageSpan.appendChild(buttonDiv)
-
 //creates Story button
     var buttonOne = document.createElement("button");
     buttonOne.innerText = "Story Mode"
     buttonOne.setAttribute("class", "col-6 btn btn-outline-light btn-lg")
     buttonDiv.appendChild(buttonOne)
-
 //creates Speed button
     var buttonTwo = document.createElement("button");
     buttonTwo.innerText = "Speed Mode"
@@ -68,7 +59,6 @@ function homeScreen(){
 // function typeWriter(){
 //          var i = 0
 //          var welcomeText = "Welcome to TypeQuest!"
-
 //              if (i < welcomeText.length){
 //                 document.getElementById("homemessage").innerText += welcomeText.charAt(i);
 //                 i++
@@ -76,40 +66,100 @@ function homeScreen(){
 //             }
 //         }
 
+//FOR STORY MODE
 
-//this function randomly selects word and displays the random word - for speed mode
-function displayWord(array){
-
-    word = array[Math.floor(Math.random() * array.length)];
-    var display = document.getElementById("display");
-    display.innerText = word;
-}
-
-//this function shuffles arrays - for story mode
 function shuffle(array){
     for (let i = array.length - 1; i > 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
 }
-
+//this function shuffles arrays - for story mode
 function displayShuffledArray(array){
     shuffle(peasantQuest[storyTracker]["array"]);
-        var i = 0
-    while (i < 3){
-        var display = document.getElementById("display");
-        display.innerText = peasantQuest[storyTracker]["array"][i];
-        word = peasantQuest[storyTracker]["array"][i];
-        i++
-        console.log(word)
+
+    var i = 0
+    var display = document.getElementById("display");
+    display.innerText = peasantQuest[storyTracker]["array"][i];
+    word = peasantQuest[storyTracker]["array"][i];
+    i++
+    console.log(word)
+
+}
+
+
+//first story displayed
+function displayMessage(){
+    document.getElementById("message").innerText = peasantQuest[storyTracker]["message"]
+}
+
+
+enterPress = 1
+
+function gameMode(){
+    displayMessage()
+    displayShuffledArray();
+    document.getElementById("input")
+        .addEventListener("keypress", function(){
+            input = event.target.value;
+            var shuffledArray = shuffle(peasantQuest[storyTracker]["array"]);
+
+            if (event.key === "Enter"){
+                checkMatchStory();
+
+                var i = 0
+                var display = document.getElementById("display");
+                display.innerText = peasantQuest[storyTracker]["array"][i];
+                word = peasantQuest[storyTracker]["array"][i];
+                i++
+                console.log(word)
+
+                displayMessage();
+
+                enterPress = enterPress+1
+                console.log(enterPress)
+                document.getElementById("inputbox").value = ""
+
+                if (enterPress%3 === 0){
+                storyTracker++
+                console.log(enterPress)
+        }
+            }
+        })
+}
+gameMode();
+
+
+
+function checkMatchStory(){
+    if (input === word){
+        score++
+           document.getElementById("number").innerText = score
+           document.getElementById("checkmark").setAttribute("src", "images/checkmark.png")
+           moveCat();
+           // moveBar();
+
+    } else if (input !== word) {
+       lives-- // consider hiding lives for speed mode
+       document.getElementById("checkmark").setAttribute("src", "images/crossout.png")
     }
 }
 
 
+
+//SPEED MODE
+//initiate timer for speed mode
 function initiateCount(){
      if (enterPress === 1){
         countDown();
      }
+}
+
+//this function randomly selects word and displays the random word - for speed mode
+function displayWord(array){
+    word = array[Math.floor(Math.random() * array.length)];
+    var display = document.getElementById("display");
+    display.innerText = word;
 }
 
 function speedMode(){
@@ -128,30 +178,6 @@ function speedMode(){
         })
 }
 
-function displayMessage(){
-    document.getElementById("message").innerText = peasantQuest[storyTracker]["message"]
-}
-displayMessage()
-displayShuffledArray();
-
-function gameMode(){
-    displayMessage()
-    document.getElementById("input")
-        .addEventListener("keypress", function(){
-            input = event.target.value;
-
-            if (event.key === "Enter"){
-                var shuffledArray = shuffle(peasantQuest[storyTracker]["array"]);
-                checkMatchStory();
-                displayShuffledArray(shuffledArray);
-                document.getElementById("inputbox").value = ""
-            }
-        })
-
-
-}
-gameMode();
-
 //this function will check if user input matches generated word
 function checkMatchSpeed(){
     if (input === word && enterPress !== 1){
@@ -167,32 +193,8 @@ function checkMatchSpeed(){
     }
 }
 
-function checkMatchStory(){
-    if (input === word){
-        score++
-           document.getElementById("number").innerText = score
-           document.getElementById("checkmark").setAttribute("src", "images/checkmark.png")
-           moveCat();
-           // moveBar();
 
-    } else if (input !== word) {
-       lives-- // consider hiding lives for speed mode
-       document.getElementById("checkmark").setAttribute("src", "images/crossout.png")
-    }
-
-     if (score === 3){
-            storyTracker++
-            score === 0
-        }
-}
-
-function keepScore(){
-
-//keeps track of player's score to know when to advance to next level
-
-}
-
-//this is the countdown timer
+//this is the countdown timer function
 function countDown(){
     var interval =
     setInterval(function(){
