ab14f1c5c2ae218a7a9e9c84b03022475897505f
“woofborn” <wolf.born@gmail.com>
2019-09-29 09:48:14 +0800

    Narrative on array of objects. Consider including in main js file.

---
+++ storyline.js
@@ -0,0 +1,125 @@
+var peasantQuest = [
+
+    {//zero
+        message: `You jolt awake to something snuffling at your ear. It feels like morning has only just broken.
+        "Meow?" the creature inquires.
+        Through the fog of your sleep-deprived mind, you recall that you don't own a cat.
+        You hope that you are dreaming.
+        You roll over, but the cat is insistent.
+        "Meow! Meowmeowmeow..."
+        Sleepily, you mumble at the cat...`,
+
+        array: ["mmf", "grr", "snnrk", "blrg", "plft"]
+    },
+
+    {//one
+        message: `Unfortunately, this only encourages the creature. It pounces on your face.
+        Enraged, you sit up and scold the cat with some choice words...`,
+
+        array: ["fudgepie", "balderdash", "shitake", "caramba", "auflauf"]
+    },
+
+    {//two
+        message: `The cat scampers away and out the door.
+        Now that you're wide awake, you decide to leave the barn as well.
+
+        Outside the barn, you find the cat waiting for you.
+        You realize that the village is oddly quiet.
+        You also realize that you have never seen this cat before.
+        You begin to have a sneaking suspicion that (1) this is no ordinary cat, (2) something has happened to your village and (3) only YOU, a nameless farm boy with nothing to his name, can save the villagers!
+        You begin to daydream of all the rewards you would receive...`,
+
+        array: ["lots of gold", "herd of cows", "black horse", "sack of potatoes", "goose feather bed"]
+    },
+
+    {//three
+        message: `"Mrrow?" queries the cat, staring at you intently.
+        Your revelry broken, you stare back at the cat.
+        The cat begins to walk, stopping every few meters to look back at you.
+        Reluctantly, you begin to follow it.
+        You decide to think of a name for the cat...`,
+
+        array: ["Kitty McMeowface", "Sir Pouncealot", "Kitten Schmitten", "Fluffy von Whiskers", "Lady Catbeth"]
+    },
+
+    {//four
+        message: `As you call each name out, the cat distinctly ignores you and walks faster.
+        You reach the edge of a forest and see a sword sticking out of a large rock.
+        You do not remember having ever seen the forest or the rock before.
+        Being an infamously forgetful person, this does not surprise you.
+        You do remember a story about an inconveniently located sword.
+        What was it's name...`,
+
+        array: ["Excalibur?", "Anduril?", "Longclaw?", "Zulfiqar?", "Durandal?"]
+    },
+
+    {//five
+        message: `While you are busy reciting famous sword names, you fail to notice someone approaching until a large shadow falls upon you.
+        "WHAT IS THIS?!" a voice roars. "A THIEF, COMING TO STEAL MY SWORD!"
+        You spin around and come face to face (or rather, face to belly) with the largest person you have ever seen.
+        He looks angry.
+        Panicking, you reach for the sword in the stone and think through what you know about warfare...`,
+
+        array: ["Pretend to be weak, that he may grow arrogant", "Subdue the enemy, without fighting", "In the midst of chaos, there is also opportunity", "The whole secret lies, in confusing the enemy", "Great results, can be achieved with small forces."]
+    },
+
+    {//six
+        message: `Intimidated by your knowledge of Sun Tzu's Art of War, the giant flees, dropping everything in his haste to get away.
+        Or perhaps it was fear of the sword you pulled out of the stone, which seems to have caught on fire.
+        You check out the items that the giant has left behind, and notice a rolled up map.
+        You open the map and peer at it, trying to remember your cardinal directions...`,
+
+        array: ["north-west", "north-east", "south-west", "south-east", "north-north-east", "south-south-west", "north-west-east", "south-north-west"]
+    },
+
+    {//seven
+        message: `Recalling that compasses have yet to be invented, you resume looking at the map and notice a big circle around a crude drawing of a castle.
+    "Perhaps all the villagers are in the castle," you think to yourself.
+    You set out in what you think is the correct direction.
+    The cat trots along beside you, making you more confident that you are heading the right way.
+    Settling in for a bit of a walk, you try to remember some marching songs...`,
+
+        array: ["The ants go marching one-by-one, hoorah! Hoorah!", "Oh, when the saints! Go marching in!", "Glory! Glory, hallelujah! The truth is marching on!", "Left-toe! Right-toe! Keep up the tempo!", "Links zwo! Links zwo! Links, zwo, drei, vier links!"]
+    },
+    {//eight
+        message: `Before too long, you find yourself approaching an immense moat.
+    In the moat, there is a boat afloat.
+    On the boat, there is a goat.
+    "Baaaa!"says the goat. "How many other words that rhyme with 'moat' can you name in 10 seconds?"`,
+
+        array: ["bloat", "coat", "gloat", "haute", "mote", "note", "oat", "quote", "rote", "wrote", "smote", "stoat", "throat", "tote", "vote", "float", "demote", "connote", "devote", "denote", "emote", "garrote", "misquote", "outvote", "promote", "remote", "rewrote"]
+    },
+
+    {//nine
+        message: `"Baaaaa humbug!" says the goat. "Your rhyming skillz are impressive. I will take you across the moat."
+        You get into the afloat boat with the goat.
+        As the boat travels across the moat, you begin to see the outline of a castle, and hear the strains of music coming from therein.
+        Inspired by the calm lapping of the water of the moat against the boat, you burst into song...`,
+
+        array: ["lalalalalalala"] //think of some songs...
+    },
+
+    {//10
+        message: `You reach the other end of the moat and thank the goat.
+        The music at the castle has gotten quite loud.
+        You hear the rhythmic beating of drums, the clinking of glasses, and what sounds like... dancing?
+        You rush through the first open door that you can find, and come upon a scene of merriment and gluttony.
+        The villagers are all there, eating, drinking, dancing, and having an all-around good time.
+        Shock turns to disbelief, and disbelief to anger.
+        You erupt...`,
+
+        array: ["abhAS$#!@TAFasb!!!", " ds;lkn;KAlkjAV@#%adbn?!"] //more keyboard mashing
+    },
+    {
+        message: `Silence descends. Someone coughs awkwardly.
+        "Sorry...we forgot to invite you to the party", someone mumbles sheepishly.
+        You take your sword out and set the curtains on fire.
+        You walk back outside to the moat and the goat in the boat.
+        You pick up the cat, get in the boat and look back at the castle.
+        You can't believe you went through all that hassle.
+        As the boat departs, you hear the villagers...`,
+
+        array: ["aarrGGGGHhh!@$%"] //more keyboard mashing, but like, screaming or something.
+    }
+
+] //THE END.
