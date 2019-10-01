4069ddc1f9032e7b8075a1d058dd3c9038237cd6
“woofborn” <wolf.born@gmail.com>
2019-10-01 13:48:50 +0800

    Revised shuffle array function.

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function gameEndScreen(){
 //FOR STORY MODE
 
 function shuffle(array){
-    for (let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i >= 0; i--) {
         let j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
       }
